0cb449934ad23082a671d09c1b10dac02e58b4d2
unknown <sajjadpoostini007@gmail.com>
2024-11-25 10:35:22 -0800

    project 1

---
+++ js/afat.js
@@ -0,0 +1,8 @@
+let firstname = "afat";
+let lastname = "taghtagh";
+let age = 40;
+let shamsi = 1403 - age;
+let miladi = shamsi + 621;
+
+document.write(`<h1>firstname is ${firstname} and lastname is ${lastname} and age is ${age}    years old
+    and shamsi born is ${shamsi} and miladi born is ${miladi}</h1> `);
---
+++ js/shahpar.js
@@ -0,0 +1,8 @@
+let firstname = "shapar";
+let lastname = "ali";
+let age = 64;
+let shamsi = 1403 - age;
+let miladi = shamsi + 621;
+
+document.write(`<h1>firstname is ${firstname} and lastname is ${lastname} and age is ${age}    years old
+    and shamsi born is ${shamsi} and miladi born is ${miladi}</h1> `);
